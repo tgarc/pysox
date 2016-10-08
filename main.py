13de44436d6bdbb00053b100383315af6ff8617e
tdos.apone <toemossgarcia@gmail.com>
2016-10-08 16:39:08 -0500

    example: list sox effects

--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 import sys
 import pysox
 
+
 def main():
+    print "Using SoX v%s" % pysox.__soxversion__
+
     assert pysox.init() == 0
-    
+
+    for (name, usage) in pysox.get_effects():
+        print "%s: %s" % (name, usage)
+
     assert pysox.quit() == 0
+
 
 if __name__ == '__main__':
     sys.exit(main())
--- pysox.py
+++ pysox.py
@@ -1,4 +1,34 @@
-from _sox import ffi as _ffi, lib as _lib
+from _sox import ffi as _ffi, lib as _sox
 
-init = _lib.sox_init
-quit = _lib.sox_quit
+__soxversion__ = _ffi.string(_sox.sox_version())
+
+init = _sox.sox_init
+quit = _sox.sox_quit
+
+
+class SoXException(Exception):
+    pass
+
+def find_effect(effect):
+    soxeff_h = _sox.sox_find_effect(effect)
+    raise SoXException("Effect '%s' not found" % effect)
+
+
+def get_effects():
+    soxeff_h_funcs = _sox.sox_get_effect_fns()
+
+    i = 0
+    sehf = soxeff_h_funcs[0]
+    while sehf != _ffi.NULL:
+        seh = sehf()
+
+        if seh.name != _ffi.NULL and seh.usage != _ffi.NULL:
+            yield _ffi.string(seh.name), _ffi.string(seh.usage)
+
+        i += 1
+        sehf = soxeff_h_funcs[i]
+        
+
+class SoXFile:
+    def __init__(self, file):
+        self._file = sox_open_read(file)
